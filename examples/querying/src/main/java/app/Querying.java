266d8b41eb50c47b716ef3ec70a6a6c6e81a54e9
Olle Törnström <toernstroem@synyx.de>
2020-01-08 21:23:28 +0100

    Add quick start makefile

--- examples/querying/src/main/java/app/Querying.java
+++ examples/querying/src/main/java/app/Querying.java
@@ -1,14 +1,26 @@
 package app;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 
+@EnableScheduling
 @SpringBootApplication
 public class Querying {
 
+    @Scheduled(fixedDelay = Long.MAX_VALUE)
+    void join() {
+
+        // OK
+    }
+
+
     public static void main(String[] args) {
 
-        SpringApplication.run(Querying.class, args);
+        new SpringApplicationBuilder(Querying.class).web(WebApplicationType.NONE).run(args);
     }
 }
