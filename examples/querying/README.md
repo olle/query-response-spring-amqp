72447f066d59f66057c4d2930ec6cdbd9dde2c01
Olle Törnström <toernstroem@synyx.de>
2020-01-08 21:05:51 +0100

    Add skeleton for querying example app
    * In a subdir there will be example projects available, demoing the
      streaming query/response-API.

---
+++ examples/querying/README.md
@@ -0,0 +1,5 @@
+Querying
+========
+
+An example of a querying service or component, using the streaming API.
+
---
+++ examples/querying/src/main/java/app/Querying.java
@@ -0,0 +1,14 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Querying {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Querying.class, args);
+    }
+}
