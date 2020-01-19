c0ffeb076596c0d8d34f13a31dd4e27501c021b9
Olle Törnström <toernstroem@synyx.de>
2020-01-19 20:48:09 +0100

    Adding RabbitMQ for examples

---
+++ examples/README.md
@@ -0,0 +1,14 @@
+Query/Response Examples
+=======================
+
+In order to show how the Query/Response API works, and provide developers with
+a setup to get some hands-on experience, we've provided a small example setup.
+
+Running the examples
+--------------------
+
+1. Make sure to open both the `querying/` and `responding/` directories in a
+   terminal window, next to each other.
+
+2. Each of the example _sides_ can be started by simply running `make`. And will
+   end by pressing `CTRL-C`.
--- examples/querying/src/main/java/app/Querying.java
+++ examples/querying/src/main/java/app/Querying.java
@@ -1,9 +1,13 @@
 package app;
 
+import com.studiomediatech.Query;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.Collections;
 
 
 @SpringBootApplication
@@ -12,7 +16,19 @@ class Querying implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Thread.currentThread().join();
+        while (true) {
+            System.out.println("Querying...");
+            queryForSciFiBooks();
+            Thread.sleep(10000);
+        }
+    }
+
+
+    private void queryForSciFiBooks() {
+
+        Query.queryFor("books/sci-fi")
+            .waitingFor(2000)
+            .orDefaults(Collections.emptyList());
     }
 
 
