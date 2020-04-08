8c6e185a5a5f8ea8314124ac55f0ecd77c1ec5eb
Olle Törnström <toernstroem@synyx.de>
2020-04-08 23:35:19 +0200

    Publish POC for statistics

--- src/main/java/com/studiomediatech/queryresponse/Statistics.java
+++ src/main/java/com/studiomediatech/queryresponse/Statistics.java
@@ -6,4 +6,11 @@ import com.studiomediatech.queryresponse.util.Logging;
 class Statistics implements Logging {
 
     // OK
+
+    public Statistics() {
+
+        ResponseBuilder.respondTo("query-response/stats")
+            .withAll()
+            .from("hello", "world!");
+    }
 }
