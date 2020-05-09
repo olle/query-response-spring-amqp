c789e1bc5b32597497ff796f0d07bd6d53183775
Olle Törnström <toernstroem@synyx.de>
2020-05-09 17:51:26 +0200

    Adds an example that only registers the stats response
    * Provides a base for investigating the issue of non-recoverability
      after lost connection with the broker.
    
      https://github.com/olle/query-response-spring-amqp/issues/19

---
+++ examples/singleresponse/src/main/java/app/StatsOnlyResponse.java
@@ -0,0 +1,27 @@
+package app;
+
+import com.studiomediatech.queryresponse.EnableQueryResponse;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+
+@SpringBootApplication
+@EnableQueryResponse
+class StatsOnlyResponse implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("> Waiting for stats queries! Press CTRL-C to exit.");
+        Thread.currentThread().join();
+    }
+
+
+    public static void main(String[] args) {
+
+        new SpringApplicationBuilder(StatsOnlyResponse.class).web(WebApplicationType.NONE).run(args);
+    }
+}
