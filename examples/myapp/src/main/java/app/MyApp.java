007b626cd4b2a5cb29ab696f37e2124cc2187f0f
Olle Törnström <toernstroem@synyx.de>
2020-05-16 11:12:33 +0200

    This app only enables Query/Response, nothing more

--- examples/myapp/src/main/java/app/MyApp.java
+++ examples/myapp/src/main/java/app/MyApp.java
@@ -5,24 +5,13 @@ import com.studiomediatech.queryresponse.EnableQueryResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @SpringBootApplication
-@RestController
 @EnableQueryResponse
 public class MyApp {
 
     public static void main(String[] args) {
 
         SpringApplication.run(MyApp.class, args);
-    }
-
-
-    @GetMapping("/")
-    public String helloWorld() {
-
-        return "Hello World!";
     }
 }
