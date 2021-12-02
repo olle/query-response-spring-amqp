0fd8dbb2aa3a26236ef329b0b157ea85aed74911
Olle Törnström <toernstroem@synyx.de>
2021-12-02 22:17:30 +0100

    Add ui2, vue3 fresh

---
+++ ui2/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav>
+    <ul>
+      <router-link to="/" tag="li">
+        <home-icon></home-icon>
+        <h2>Overview</h2>
+      </router-link>
+      <router-link class="disabled" to="/queries" tag="li">
+        <queries-icon></queries-icon>
+        <h2>Queries Insight</h2>
+      </router-link>
+      <router-link class="disabled" to="topology" tag="li">
+        <topology-icon></topology-icon>
+        <h2>Q/R Topology</h2>
+      </router-link>
+      <router-link class="disabled" to="/live" tag="li">
+        <live-icon></live-icon>
+        <h2>Live</h2>
+      </router-link>
+      <router-link class="disabled" to="/logging" tag="li">
+        <logging-icon></logging-icon>
+        <h2>Logs</h2>
+      </router-link>
+      <router-link to="/cli" tag="li">
+        <prompt-icon></prompt-icon>
+        <h2>Query CLI</h2>
+      </router-link>
+    </ul>
+    <ul>
+      <router-link class="disabled" to="settings" tag="li">
+        <settings-icon></settings-icon>
+        <h2>Settings</h2>
+      </router-link>
+      <qr-color-scheme-toggle></qr-color-scheme-toggle>
+    </ul>
+  </nav>
+  <main>
+    <router-view></router-view>
+    <footer>
+      &copy; Copyright
+      <current-year></current-year>
+      Olle Törnström -
+      <a href="https://github.com/olle/query-response-spring-amqp/blob/master/LICENSE">The Apache License 2.0</a>
+    </footer>
+  </main>
+</template>
+
+<script setup></script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
