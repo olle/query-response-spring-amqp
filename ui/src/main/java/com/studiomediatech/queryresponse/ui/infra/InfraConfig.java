6ca41d2cf9e477f2839220de2864e15c4ad5123a
Olle Törnström <olle@studiomediatech.com>
2023-03-26 14:15:37 +0200

    Add WebSocket adapter

--- ui/src/main/java/com/studiomediatech/queryresponse/ui/infra/InfraConfig.java
+++ ui/src/main/java/com/studiomediatech/queryresponse/ui/infra/InfraConfig.java
@@ -1,21 +1,39 @@
 package com.studiomediatech.queryresponse.ui.infra;
 
+import java.util.Optional;
+
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+import com.studiomediatech.queryresponse.ui.app.adapter.EventEmitterAdapter;
+import com.studiomediatech.queryresponse.ui.app.adapter.WebSocketApiAdapter;
 import com.studiomediatech.queryresponse.ui.app.telemetry.TelemetryService;
 
 /**
  * Configuration for components of the application, with adapters for the platform.
- *
  */
 @Configuration
 @ComponentScan(basePackageClasses = InfraConfig.class)
 public class InfraConfig {
 
     @Bean
-    public TelemetryService queryResponseTelemetryService() {
-        return new TelemetryService();
+    @Primary
+    TaskScheduler taskScheduler() {
+        return new ThreadPoolTaskScheduler();
+    }
+
+    @Bean
+    EventEmitterAdapter eventEmitter(TaskScheduler scheduler, ApplicationEventPublisher publisher) {
+        return new AsyncEventEmitter(scheduler, publisher);
+    }
+
+    @Bean
+    public TelemetryService telemetryService(Optional<WebSocketApiAdapter> optionalWebSocketApiAdapter) {
+        return new TelemetryService(optionalWebSocketApiAdapter.orElse(WebSocketApiAdapter.empty()));
     }
 }
