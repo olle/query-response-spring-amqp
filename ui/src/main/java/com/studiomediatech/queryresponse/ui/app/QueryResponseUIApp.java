d41fc072db83baad9e956fab88babe3fec26dc87
Olle Törnström <olle@studiomediatech.com>
2023-03-26 09:25:24 +0200

    Use more decoupled app structure with ports/adapters

--- ui/src/main/java/com/studiomediatech/queryresponse/ui/app/QueryResponseUIApp.java
+++ ui/src/main/java/com/studiomediatech/queryresponse/ui/app/QueryResponseUIApp.java
@@ -1,18 +1,27 @@
-package com.studiomediatech.queryresponse.ui;
+package com.studiomediatech.queryresponse.ui.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 import com.studiomediatech.queryresponse.EnableQueryResponse;
+import com.studiomediatech.queryresponse.ui.messaging.Messaging;
 
-@SpringBootApplication
-@EnableQueryResponse
-@EnableScheduling
 @EnableWebSocket
+@EnableScheduling
+@EnableQueryResponse
+@SpringBootApplication
 public class QueryResponseUIApp {
+
     public static void main(String[] args) {
         SpringApplication.run(QueryResponseUIApp.class);
+    }
+
+    @Configuration
+    @Import({ Messaging.class })
+    static class Setup {
     }
 }
--- ui/src/main/java/com/studiomediatech/queryresponse/ui/service/QueryResponseStatsService.java
+++ ui/src/main/java/com/studiomediatech/queryresponse/ui/service/QueryResponseStatsService.java
@@ -9,12 +9,24 @@ import com.studiomediatech.queryresponse.util.Logging;
 @Service
 public class QueryResponseStatsService implements Logging, StatsHandlerAdapter {
 
+    // private final NodesRepository nodesRepository;
+    //
+    // public QueryResponseStatsService(NodesRepository nodesRepository) {
+    // this.nodesRepository = nodesRepository;
+    // }
+
     @Override
     public void handleConsumed(Stats stats) {
+
+        log().info("Consumed stats with {} elements", stats.elements().size());
+
+        // stats.elements().stream().map(Stat::uuid).map(UUID::fromString).map(Node::from).forEach(nodesRepository::save);
+
         stats.elements().stream().filter(s -> s.timestamp() != null).map(Stat::toString)
                 .forEach(str -> log().debug("STAT VALUE: {}", str));
 
         stats.elements().stream().filter(s -> s.timestamp() == null).map(Stat::toString)
                 .forEach(str -> log().debug("STAT INFO: {}", str));
+
     }
 }
