f0591bc6a6a85495d15b8f75de9245963d1aba9d
Olle Törnström <olle@studiomediatech.com>
2023-04-15 12:09:20 +0200

    Enable web sockets

--- ui/src/main/java/com/studiomediatech/queryresponse/ui/api/WebSocketConfig.java
+++ ui/src/main/java/com/studiomediatech/queryresponse/ui/api/WebSocketConfig.java
@@ -1,26 +1,22 @@
-package com.studiomediatech.queryresponse.ui;
+package com.studiomediatech.queryresponse.ui.api;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-import com.studiomediatech.queryresponse.ui.api.WebSocketApiHandlerPort;
-
 @Order(100)
 @Configuration
-class ConfigureWebSocket implements WebSocketConfigurer {
+class WebSocketConfig implements WebSocketConfigurer {
 
     private final WebSocketApiHandlerPort webSocketHandler;
 
-    public ConfigureWebSocket(WebSocketApiHandlerPort webSocketHandler) {
-
+    public WebSocketConfig(WebSocketApiHandlerPort webSocketHandler) {
         this.webSocketHandler = webSocketHandler;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-
         // TODO: DO NOT ALLOW ORIGINS * !!!
         registry.addHandler(webSocketHandler, "/ws").setAllowedOrigins("*");
     }
