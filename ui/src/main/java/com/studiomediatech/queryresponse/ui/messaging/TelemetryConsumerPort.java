bbe24b8baf71fb803055118ffba7f7c5c0413609
Olle Törnström <olle@studiomediatech.com>
2023-04-15 11:54:53 +0200

    Better names

--- ui/src/main/java/com/studiomediatech/queryresponse/ui/messaging/TelemetryConsumerPort.java
+++ ui/src/main/java/com/studiomediatech/queryresponse/ui/messaging/TelemetryConsumerPort.java
@@ -9,40 +9,40 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.studiomediatech.queryresponse.ui.app.adapter.MessageHandlerAdapter;
+import com.studiomediatech.queryresponse.ui.app.adapter.TelemetryHandlerAdapter;
 import com.studiomediatech.queryresponse.util.Logging;
 
 /**
- * Consumes Query/Response messages from the internal topics, and directly delegates for handling via the abstract
- * adapter.
+ * Consumes Query/Response telemetry messages from the internal topics, and directly delegates for handling via the
+ * abstract adapter.
  */
 @Component
-class MessageConsumerPort implements Logging {
+class TelemetryConsumerPort implements Logging {
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
     /**
      * {@link AcknowledgeMode#NONE}
      */
-    private static final String ACK_MODE = "NONE";
-    private static final String CONSUMERS_MIN = "3";
-    private static final String CONSUMERS_MAX = "11";
+    private static final String NONE = "NONE";
+    private static final String CONSUMERS_MIN = "1";
+    private static final String CONSUMERS_MAX = "7";
 
-    private final MessageHandlerAdapter adapter;
+    private final TelemetryHandlerAdapter adapter;
 
-    public MessageConsumerPort(Optional<MessageHandlerAdapter> maybe) {
-        this.adapter = maybe.orElse(MessageHandlerAdapter.empty());
+    public TelemetryConsumerPort(Optional<TelemetryHandlerAdapter> maybe) {
+        this.adapter = maybe.orElse(TelemetryHandlerAdapter.empty());
     }
 
     @RabbitListener(//
             queues = "#{@" + MessagingConfig.QUERY_RESPONSE_STATS_QUEUE_BEAN + "}", //
-            ackMode = ACK_MODE, concurrency = CONSUMERS_MIN + "-" + CONSUMERS_MAX)
+            ackMode = NONE, concurrency = CONSUMERS_MIN + "-" + CONSUMERS_MAX)
     void onQueryResponseStats(Message message) {
         try {
             Stats stats = MAPPER.readValue(message.getBody(), Stats.class);
             adapter.handleConsumed(stats);
         } catch (RuntimeException | IOException ex) {
-            log().error("Failed to consumed stats", ex);
+            log().error("Failed to consumed telemetry message", ex);
         }
     }
 }
