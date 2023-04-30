46eb233904045af07e2c7772e67cc7373b3a2b79
Olle Törnström <olle@studiomediatech.com>
2023-04-30 10:42:19 +0200

    Add binding for modern routing key

--- ui/src/main/java/com/studiomediatech/queryresponse/ui/messaging/MessagingConfig.java
+++ ui/src/main/java/com/studiomediatech/queryresponse/ui/messaging/MessagingConfig.java
@@ -14,31 +14,39 @@ import com.studiomediatech.queryresponse.EnableQueryResponse;
 import com.studiomediatech.queryresponse.QueryResponseTopicExchange;
 
 /**
- * Configuration for ports that enable messaging, specifically AMQP including Query/Response.
+ * Configuration for ports that enable messaging, specifically AMQP including
+ * Query/Response.
  */
 @Configuration
 @EnableQueryResponse
 @ComponentScan(basePackageClasses = MessagingConfig.class)
 public class MessagingConfig {
 
-    static final String QUERY_RESPONSE_STATS_QUEUE_BEAN = "queryResponseStatsQueue";
-    static final String QUERY_RESPONSE_QUERIES_QUEUE_BEAN = "queryResponseQueriesQueue";
-    static final String QUERY_RESPONSE_STATS_QUEUE_BINDING_BEAN = "queryResponseQueriesQueueBinding";
-    static final String QUERY_RESPONSE_INTERNAL_STATS_ROUTING_KEY = "query-response/internal/stats";
+	static final String QUERY_RESPONSE_STATS_QUEUE_BEAN = "queryResponseStatsQueue";
+	static final String QUERY_RESPONSE_QUERIES_QUEUE_BEAN = "queryResponseQueriesQueue";
 
-    @Bean
-    ConnectionNameStrategy connectionNameStrategy(Environment env) {
-        return connectionFactory -> env.getProperty("spring.application.name", "query-response-ui");
-    }
+	protected static final String QUERY_RESPONSE_STATS_BINDING = "queryResponseStatsBinding";
+	protected static final String QUERY_RESPONSE_TELEMETRY_BINDING = "queryResponseTelemetryBinding";
 
-    @Bean(QUERY_RESPONSE_STATS_QUEUE_BEAN)
-    Queue queryResponseStatsQueue() {
-        return new AnonymousQueue();
-    }
+	@Bean
+	ConnectionNameStrategy connectionNameStrategy(Environment env) {
+		return connectionFactory -> env.getProperty("spring.application.name", "query-response-ui");
+	}
 
-    @Bean(QUERY_RESPONSE_STATS_QUEUE_BINDING_BEAN)
-    Binding queryResponseStatsQueueBinding(QueryResponseTopicExchange queryResponseTopicExchange) {
-        return BindingBuilder.bind(queryResponseStatsQueue()).to(queryResponseTopicExchange)
-                .with(QUERY_RESPONSE_INTERNAL_STATS_ROUTING_KEY);
-    }
+	@Bean(QUERY_RESPONSE_STATS_QUEUE_BEAN)
+	Queue queryResponseStatsQueue() {
+		return new AnonymousQueue();
+	}
+
+	@Bean(QUERY_RESPONSE_STATS_BINDING)
+	Binding queryResponseStatsQueueBinding(QueryResponseTopicExchange queryResponseTopicExchange) {
+		return BindingBuilder.bind(queryResponseStatsQueue()).to(queryResponseTopicExchange)
+				.with("query-response/internal/stats");
+	}
+
+	@Bean(QUERY_RESPONSE_TELEMETRY_BINDING)
+	Binding queryResponseTelemetryQueueBinding(QueryResponseTopicExchange queryResponseTopicExchange) {
+		return BindingBuilder.bind(queryResponseStatsQueue()).to(queryResponseTopicExchange)
+				.with("query-response/internal/telemetry");
+	}
 }
