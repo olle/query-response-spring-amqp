71c94fa0e1dc267aee047d14579532d47df3b97d
Olle Törnström <olle@studiomediatech.com>
2024-01-07 13:16:18 +0100

    Interrim

--- ui/src/main/java/com/studiomediatech/queryresponse/ui/messaging/MessagingConfig.java
+++ ui/src/main/java/com/studiomediatech/queryresponse/ui/messaging/MessagingConfig.java
@@ -14,39 +14,38 @@ import com.studiomediatech.queryresponse.EnableQueryResponse;
 import com.studiomediatech.queryresponse.QueryResponseTopicExchange;
 
 /**
- * Configuration for ports that enable messaging, specifically AMQP including
- * Query/Response.
+ * Configuration for ports that enable messaging, specifically AMQP including Query/Response.
  */
 @Configuration
 @EnableQueryResponse
 @ComponentScan(basePackageClasses = MessagingConfig.class)
 public class MessagingConfig {
 
-	static final String QUERY_RESPONSE_STATS_QUEUE_BEAN = "queryResponseStatsQueue";
-	static final String QUERY_RESPONSE_QUERIES_QUEUE_BEAN = "queryResponseQueriesQueue";
+    static final String QUERY_RESPONSE_STATS_QUEUE_BEAN = "queryResponseStatsQueue";
+    static final String QUERY_RESPONSE_QUERIES_QUEUE_BEAN = "queryResponseQueriesQueue";
 
-	protected static final String QUERY_RESPONSE_STATS_BINDING = "queryResponseStatsBinding";
-	protected static final String QUERY_RESPONSE_TELEMETRY_BINDING = "queryResponseTelemetryBinding";
+    protected static final String QUERY_RESPONSE_STATS_BINDING = "queryResponseStatsBinding";
+    protected static final String QUERY_RESPONSE_TELEMETRY_BINDING = "queryResponseTelemetryBinding";
 
-	@Bean
-	ConnectionNameStrategy connectionNameStrategy(Environment env) {
-		return connectionFactory -> env.getProperty("spring.application.name", "query-response-ui");
-	}
+    @Bean
+    ConnectionNameStrategy connectionNameStrategy(Environment env) {
+        return connectionFactory -> env.getProperty("spring.application.name", "query-response-ui");
+    }
 
-	@Bean(QUERY_RESPONSE_STATS_QUEUE_BEAN)
-	Queue queryResponseStatsQueue() {
-		return new AnonymousQueue();
-	}
+    @Bean(QUERY_RESPONSE_STATS_QUEUE_BEAN)
+    Queue queryResponseStatsQueue() {
+        return new AnonymousQueue();
+    }
 
-	@Bean(QUERY_RESPONSE_STATS_BINDING)
-	Binding queryResponseStatsQueueBinding(QueryResponseTopicExchange queryResponseTopicExchange) {
-		return BindingBuilder.bind(queryResponseStatsQueue()).to(queryResponseTopicExchange)
-				.with("query-response/internal/stats");
-	}
+    @Bean(QUERY_RESPONSE_STATS_BINDING)
+    Binding queryResponseStatsQueueBinding(QueryResponseTopicExchange queryResponseTopicExchange) {
+        return BindingBuilder.bind(queryResponseStatsQueue()).to(queryResponseTopicExchange)
+                .with("query-response/internal/stats");
+    }
 
-	@Bean(QUERY_RESPONSE_TELEMETRY_BINDING)
-	Binding queryResponseTelemetryQueueBinding(QueryResponseTopicExchange queryResponseTopicExchange) {
-		return BindingBuilder.bind(queryResponseStatsQueue()).to(queryResponseTopicExchange)
-				.with("query-response/internal/telemetry");
-	}
+    @Bean(QUERY_RESPONSE_TELEMETRY_BINDING)
+    Binding queryResponseTelemetryQueueBinding(QueryResponseTopicExchange queryResponseTopicExchange) {
+        return BindingBuilder.bind(queryResponseStatsQueue()).to(queryResponseTopicExchange)
+                .with("query-response/internal/telemetry");
+    }
 }
