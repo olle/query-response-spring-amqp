b66b64a25212ae998d8582e95f0e90170550d386
Olle Törnström <olle@studiomediatech.com>
2024-11-30 17:55:04 +0100

    Extend logging asserts

--- src/test/java/com/studiomediatech/queryresponse/util/LoggableTest.java
+++ src/test/java/com/studiomediatech/queryresponse/util/LoggableTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
+import org.springframework.amqp.core.MessageBuilder;
 
 class LoggableTest {
 
@@ -14,6 +15,15 @@ class LoggableTest {
                 assertThat(logger()).isNotNull().isInstanceOfAny(Logger.class);
             }
         }).test();
+    }
+
+    @Test
+    void ensureRedactedMessageOnlyLogsByteLengthOfBody() throws Exception {
+
+        var message = MessageBuilder.withBody("{this-is-redacted}".getBytes()).setReplyTo("reply-to").build();
+
+        assertThat(Loggable.toStringRedacted(message)).doesNotContain("{this-is-redacted}").contains("Body:[18]")
+                .contains("replyTo=reply-to");
     }
 
 }
